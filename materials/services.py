5127091d86e821e92ce30337e8bd9475b11df805
Ilya Grig <ilia.grigoriev2018@yandex.ru>
2024-08-08 19:26:25 +0400

    cool14

--- materials/services.py
+++ materials/services.py
@@ -12,8 +12,17 @@ def convert_rub_to_dollars(sum_of_payments):
 
 
 def create_stripe_price(sum_of_payments):
-    stripe.Price.create(
+    return stripe.Price.create(
         currency="usd",
         unit_amount=sum_of_payments * 100,
         product_data={"name": "Payments"},
     )
+
+
+def create_stripe_session(price):
+    session = stripe.checkout.Session.create(
+        success_url="https://127.0.0.1:8000/",
+        line_items=[{"price": price.get("id"), "quantity": 1}],
+        mode="payment",
+    )
+    return session.get("id"), session.get("url")
--- users/views.py
+++ users/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
-from users.serializers import UserSerializer,PaymentsSerializers
+
+from materials.services import convert_rub_to_dollars, create_stripe_price, create_stripe_session
+from users.serializers import UserSerializer, PaymentsSerializers
 
 from users.models import User, Payments
 
@@ -23,6 +25,11 @@ class PaymentsCreateAPIView(CreateAPIView):
     queryset = Payments.objects.all()
 
     def perform_create(self, serializer):
-        user = serializer.save(is_active=True)
-        user.set_password(user.password)
-        user.save()
+        payment = serializer.save(user=self.request.user)
+        amount_in_dollars = convert_rub_to_dollars(payment.sum_of_payments)
+        price = create_stripe_price(amount_in_dollars)
+        session_id, payment_link = create_stripe_session(price)
+        payment.session_id = session_id
+        payment_link = payment_link
+        payment.save()
+
